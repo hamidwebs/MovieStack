597acd4b7990d27a2efd93a39eafe76bf1cd28db
Hamid Raza <itmughal2023@gmail.com>
2024-05-17 21:33:42 +0500

    Add Vercel Pkg

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import './index.scss'
 import store from './store/store'
 import { Provider } from 'react-redux'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
+    <SpeedInsights />
   </Provider>
 )
